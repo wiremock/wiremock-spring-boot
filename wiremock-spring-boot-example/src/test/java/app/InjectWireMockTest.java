8665fe42dd806a42f436a7ffc76b32474a843706
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-09 21:23:53 +0200

    chore: test cases

---
+++ wiremock-spring-boot-example/src/test/java/app/InjectWireMockTest.java
@@ -0,0 +1,30 @@
+package app;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock
+class InjectWireMockTest {
+
+  @InjectWireMock("wiremock")
+  private WireMockServer wireMockServer;
+
+  @Value("${wiremock.server.baseUrl}")
+  private String wiremockUrl;
+
+  @Test
+  void returnsTodos() {
+    this.wireMockServer.stubFor(get("/ping").willReturn(aResponse().withStatus(200)));
+
+    RestAssured.when().get(this.wiremockUrl + "/ping").then().statusCode(200);
+  }
+}
