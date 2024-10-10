d8a6206b51ee53ded647830fee712ca1d351a831
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-10 18:04:37 +0200

    chore: formatting

--- src/main/java/org/wiremock/spring/InjectWireMock.java
+++ src/main/java/org/wiremock/spring/InjectWireMock.java
@@ -6,19 +6,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Injects WireMock instance previously configured on the class or field level
- * with {@link ConfigureWireMock}.
+ * Injects WireMock instance previously configured on the class or field level with {@link
+ * ConfigureWireMock}.
  *
  * @author Maciej Walkowiak
  */
-@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface InjectWireMock {
 
-	/**
-	 * The name of WireMock instance to inject.
-	 *
-	 * @return the name of WireMock instance to inject.
-	 */
-	String value() default "wiremock";
+  /**
+   * The name of WireMock instance to inject.
+   *
+   * @return the name of WireMock instance to inject.
+   */
+  String value() default "wiremock";
 }
--- wiremock-spring-boot-example/src/test/java/app/InjectNamedWireMockTest.java
+++ wiremock-spring-boot-example/src/test/java/app/InjectNamedWireMockTest.java
@@ -3,6 +3,8 @@ package app;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,24 +12,20 @@ import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
 import org.wiremock.spring.InjectWireMock;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-
-import io.restassured.RestAssured;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EnableWireMock(@ConfigureWireMock(name = "mywiremock"))
 class InjectDefaultWireMockTest {
 
-	@InjectWireMock("mywiremock")
-	private WireMockServer wireMockServer;
+  @InjectWireMock("mywiremock")
+  private WireMockServer wireMockServer;
 
-	@Value("${wiremock.server.baseUrl}")
-	private String wiremockUrl;
+  @Value("${wiremock.server.baseUrl}")
+  private String wiremockUrl;
 
-	@Test
-	void returnsTodos() {
-		this.wireMockServer.stubFor(get("/ping").willReturn(aResponse().withStatus(200)));
+  @Test
+  void returnsTodos() {
+    this.wireMockServer.stubFor(get("/ping").willReturn(aResponse().withStatus(200)));
 
-		RestAssured.when().get(this.wiremockUrl + "/ping").then().statusCode(200);
-	}
+    RestAssured.when().get(this.wiremockUrl + "/ping").then().statusCode(200);
+  }
 }
