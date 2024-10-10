bb9ee82a13f092ae985f9e62f78e6514dc2816e8
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-10 17:59:44 +0200

    fix: looking for mocks under directory before classpath (refs #49)

--- wiremock-spring-boot-example/src/test/java/app/FilesUnderClasspathTest.java
+++ wiremock-spring-boot-example/src/test/java/app/FilesUnderClasspathTest.java
@@ -2,41 +2,45 @@ package app;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import app.todoclient.Todo;
-import app.todoclient.TodoClient;
-import java.util.List;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
 
 @SpringBootTest
-@EnableWireMock({
-  @ConfigureWireMock(
-      name = "todo-client",
-      baseUrlProperties = "todo-client.url",
-      filesUnderClasspath = "custom-location")
-})
+@EnableWireMock({@ConfigureWireMock(filesUnderClasspath = "custom-location")})
 class FilesUnderClasspathTest {
 
-  @Autowired private TodoClient todoClient;
+  @Value("${wiremock.server.baseUrl}")
+  private String wireMockServerUrl;
 
   @Test
-  void usesStubFilesFromCustomLocation() {
-    final List<Todo> todos = this.todoClient.findAll();
-    assertThat(todos).isNotNull().hasSize(2);
-    assertThat(todos.get(0))
-        .satisfies(
-            todo -> {
-              assertThat(todo.id()).isEqualTo(1);
-              assertThat(todo.title()).isEqualTo("custom location todo 1");
-            });
-    assertThat(todos.get(1))
-        .satisfies(
-            todo -> {
-              assertThat(todo.id()).isEqualTo(2);
-              assertThat(todo.title()).isEqualTo("custom location todo 2");
-            });
+  void test() {
+    RestAssured.baseURI = this.wireMockServerUrl;
+    final String actual =
+        RestAssured.when()
+            .get("/classpathmappings")
+            .then()
+            .statusCode(200)
+            .extract()
+            .asPrettyString();
+    assertThat(actual)
+        .isEqualToIgnoringWhitespace(
+            """
+				[
+				    {
+				        "id": 1,
+				        "title": "custom location todo 1",
+				        "userId": 1
+				    },
+				    {
+				        "id": 2,
+				        "title": "custom location todo 2",
+				        "userId": 1
+				    }
+				]
+				""");
   }
 }
