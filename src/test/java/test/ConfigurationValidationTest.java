c5af3b7e074230bf117dc7e1f9983eade61f9610
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-11-08 10:55:56 +0100

    chore: using splitted convention plugins (#99)

--- src/test/java/test/ConfigurationValidationTest.java
+++ src/test/java/test/ConfigurationValidationTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextCustomizer;
